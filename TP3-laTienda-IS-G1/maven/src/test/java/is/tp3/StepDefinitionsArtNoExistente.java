6b6bbb92a286655384711f5838244ec3b6094719
sabrinaced28 <sabrinacedron28@gmail.com>
2023-11-28 00:09:41 -0300

    Cambios punto cuatro

--- TP3-laTienda-IS-G1/maven/src/test/java/is/tp3/StepDefinitionsArtNoExistente.java
+++ TP3-laTienda-IS-G1/maven/src/test/java/is/tp3/StepDefinitionsArtNoExistente.java
@@ -1,13 +1,51 @@
 package is.tp3;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import is.tp3.domain.Articulo;
+import is.tp3.repositories.RepositorioArticulo;
+import is.tp3.services.ServicioBuscarArticulo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
 
 public class StepDefinitionsArtNoExistente {
-    @Then("se muestra mensaje de error {string}")
-    public void se_muestra_mensaje_de_error(String mensajeError) {
-        String mensaje = mensajeError;
-        System.out.println(mensaje);
+    Articulo articulo;
+    public RepositorioArticulo repositorioMock;
+    @Before
+    public void setUp(){
+        repositorioMock = mock(RepositorioArticulo.class);
+    }
+
+    @Given("el siguiente articulo")
+    public void el_siguiente_articulo(List<Map<String,String>> arti ) {
+        ArrayList<Articulo> art= new ArrayList<Articulo>();
+        for(Map<String,String> item:arti){
+            Integer codigo= Integer.parseInt(item.get("Codigo"));
+            String marca= item.get("Marca");
+            String descripcion=item.get("Descripcion");
+            Integer precio= Integer.parseInt(item.get("Precio"));
+            art.add(new Articulo(codigo,marca,descripcion,precio));
+        }
+    articulo = art.get(0);
 
     }
+    @When("ingresa el codigo {int}")
+    public void ingresa_el_codigo(Integer Codigo){
+        articulo=null;
+        ServicioBuscarArticulo buscar =new ServicioBuscarArticulo(repositorioMock);
+        articulo= buscar.buscarArticulo(Codigo);
+    }
+    @Then("se muestra mensaje de error")
+    public void se_muestra_mensaje_de_error() {
+            assertNull(articulo,"El articulo no existe");
+    }
+
 
 }
--- TP3-laTienda-IS-G1/maven/src/test/resources/is/tp3/ArticuloNoExistente.feature
+++ TP3-laTienda-IS-G1/maven/src/test/resources/is/tp3/ArticuloNoExistente.feature
@@ -3,7 +3,10 @@ Feature: comprobar existencia de un articulo
   Quiero verificar la disponibilidad de un articulo
   Para agregarlo a la venta
 
-Scenario: Error al agregar articulo inexistente
-Given una venta en proceso
-When ingreso el codigo 1234
-Then se muestra mensaje de error "Articulo no existente"
+Scenario: Error al buscar articulo inexistente
+  Given una venta en proceso
+  Given el siguiente articulo
+  | Codigo | Marca  | Descripcion    | Precio |
+  | 124    | Adidas | Indumentaria   | 30000  |
+  When ingresa el codigo 1234
+  Then se muestra mensaje de error
